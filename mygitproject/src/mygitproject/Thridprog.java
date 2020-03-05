20bcee394f3d009fb56353900c9cd00ad91c86ce
Sravani <Sravani@LAPTOP-1CFCF6HD>
2020-03-04 19:23:07 -0500

    first commit

---
+++ mygitproject/src/mygitproject/Thridprog.java
@@ -0,0 +1,11 @@
+package mygitproject;
+
+public class Thridprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("third program");
+
+	}
+
+}
