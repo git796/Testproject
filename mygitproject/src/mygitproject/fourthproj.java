11a759f2f72c665d4119255ded5fc1362bd5014b
Sravani <Sravani@LAPTOP-1CFCF6HD>
2020-03-04 19:47:11 -0500

    forth.java program added.

---
+++ mygitproject/src/mygitproject/fourthproj.java
@@ -0,0 +1,14 @@
+package mygitproject;
+
+public class fourthproj {
+
+	public fourthproj() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
